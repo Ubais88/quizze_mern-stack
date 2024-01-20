030b08c51556903a32532f42bda05aa7d1d9efcf
Ubais88 <moubais2143@gmail.com>
2024-01-20 10:14:57 +0530

    create quiz modal added

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,12 +3,13 @@ import Sidebar from '../../components/sidebar/Sidebar'
 import Dashboard from '../../components/dashboard/Dashboard'
 import styles from "./Home.module.css"
 import Analytics from '../../components/analytics/Analytics'
+import Createquiz from '../../components/createQuiz/Createquiz'
 
 const Home = () => {
   return (
     <div className={styles.homeContainer}>
         <Sidebar/>
-        <Analytics/>
+        <Createquiz/>
     </div>
   )
 }
